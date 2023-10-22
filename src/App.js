db699510705f93b9260482c1a9215593fe39bcba
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-10-22 18:39:04 +0900

    component 컴포넌트 사용 방법 실습 React는 사용자가 정의한 태그를 만드는 기술이며, 이 태그를 컴포넌트라고 부른다. 이번 시간에는 컴포넌트를 함수 문법으로 만들었지만 클래스로도 선언이 가능하며, export 선언하였으므로 다른 사람이 내 코드를 사용할 수도, 내가 다른 사람의 코드를 바로 가져와 사용할 수도 있다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
 import logo from "./logo.svg";
 import "./App.css";
+// 함수를 사용하여 사용자 정의 태그를 만든다.
+// React에서 사용자 정의 태그를 위한 함수를 만들때에는
+// 반드시 이름이 대문자로 시작하여야 한다.
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+const Nav = () => {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">HTML</a>
+        </li>
+        <li>
+          <a href="/read/2">CSS</a>
+        </li>
+        <li>
+          <a href="/read/3">JS</a>
+        </li>
+      </ol>
+    </nav>
+  );
+};
+
+const Article = () => {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, Web
+    </article>
+  );
+};
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  );
 }
 
 export default App;
